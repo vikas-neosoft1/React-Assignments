08a9854f847cd71132dc30aca50b6deacad0622e
vikas <vikas.s@neosoftmail.com>
2022-06-13 20:01:49 +0530

    initial commit

---
+++ src/services/Productservice.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+ const URL = "http://localhost:3001/products/";
+ 
+ function getAllProducts(){
+      return axios.get(URL);
+ }
+  
+function getProductById(id) {
+      return axios.get(`${URL}${id}`);
+} 
+  
+function addProduct(data) {
+     return axios.post(`${URL}`, data);
+}  
+
+ function editProductById(id,data) {
+    return axios.post(`${URL}${id}`,data);
+} 
+ 
+function updateProduct(id, data) {
+      return axios.post(`${URL}${id}`, data);
+} 
+ 
+
+function deleteProductById(id) {
+    return axios.delete(`${URL}${id}`);
+}  
+  
+  
+  
+ export { getAllProducts, getProductById, deleteProductById,addProduct,editProductById, updateProduct }; 
